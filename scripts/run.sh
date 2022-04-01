dd2f17b759ef15d7aa8eb2e2fb3235bf55c1d685
ArcKenimuZ <arckenimuz@gmail.com>
2022-04-01 02:13:53 +0100

    Implement combine reachability calculation

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,4 @@
+rm -rf target/
 javac -d target/ HardWorkers.java; 
 javac -d target/ -classpath lib/javassist.jar:. ist/meic/pava/UsingMethodCombination.java; 
 java -classpath target/:lib/javassist.jar:. ist.meic.pava.UsingMethodCombination HardWorkers;
