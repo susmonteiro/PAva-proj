0379334731f90d231a019d7f76b4f21f9adb0011
susmonteiro <s.moreno.monteiro@tecnico.ulisboa.pt>
2022-04-16 22:02:38 +0100

    implement defmethod syntax

--- proj2/methodCombination.jl
+++ proj2/methodCombination.jl
@@ -21,4 +21,34 @@ macro defgeneric(form, symbol=:standard)
     end
 end
 
+struct Method
+    name
+    parameters
+    body
+    native_function
+end
+
+(f::Method)(x...) = f.native_function(x...)
+
+# todo take care of optional symbol
+macro defmethod(form)
+        #= if symbol != :before && symbol != :primary && symbol != :after
+        # ? best exception to throw
+        throw(ArgumentError("Symbol must be \"before\", \"primary\" or \"after\""))
+    end =#
+    let name = form.args[1].args[1],
+        parameters = form.args[1].args[2:end],
+        body = form.args[2]
+    esc(:($(name) =
+        Method(
+            $(QuoteNode(name)),
+            $((parameters...,)),
+            $(QuoteNode(body)),
+            ($(parameters...),) -> $body
+        )))
+    end
+end
+
 @defgeneric add(x, y)
+
+@defmethod add(x::Int, y::Int) = x + y
