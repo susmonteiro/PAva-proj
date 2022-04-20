194ce082ed6ffe2a7b80f5b6ff555b92088d61b1
susmonteiro <s.moreno.monteiro@tecnico.ulisboa.pt>
2022-04-20 19:46:54 +0100

    start implementing method combination

---
+++ proj2/help.md
@@ -0,0 +1,6 @@
+Stuff that might help:
+
+```
+julia> Tuple{Int,AbstractString} <: Tuple{Real,Any}
+true
+```
--- proj2/methodCombination.jl
+++ proj2/methodCombination.jl
@@ -15,12 +15,44 @@ end
 
 (f::Generic)(args...) = combineMethods(f, args...)
 
+function no_applicable_method(f::Generic, args...)
+    # todo print types like (x,y) instead of Tuple{x,y}
+    error("No applicable method for arguments $args of types $(typeof(args))")
+end
+
+
+# todo should be done with multiple dispatch instead (standard, tuple, etc should be objects)
 function combineMethods(genericMethod::Generic, arguments...)
-    for method in genericMethod.methods
-        if applicable(method.native_function, arguments...)
-            println(method.native_function(arguments...))
+    if genericMethod.qualifier == :standard
+        standardCombination(genericMethod, arguments...)
+    else
+        tupleCombination(genericMethod, arguments...)
+    end
+end
+
+function standardCombination(genericMethod::Generic, arguments...)
+    getApplicableMethods(genericMethod.methods, :before, arguments...)
+    found = getApplicableMethods(genericMethod.methods, :primary, arguments...)
+    if !found
+        no_applicable_method(genericMethod, arguments...)
+    end
+    getApplicableMethods(genericMethod.methods, :after, arguments...)
+end
+
+function tupleCombination(genericMethod::Generic, arguments...)
+    # todo
+end
+
+# todo probably we should have 3 functions, each for the corresponding qualifier and then call them using multiple dispatch
+function getApplicableMethods(methods, qualifier, arguments...)
+    found = false
+    for method in methods
+        if method.qualifier == qualifier && applicable(method.native_function, arguments...)
+            method.native_function(arguments...)
+            found = true
         end
     end
+    return found
 end
 
 macro defgeneric(form, qualifier=:standard)
@@ -64,3 +96,18 @@ macro defmethod(qualifier, form)
         ))))
     end
 end
+
+
+
+# todo remove this
+@defgeneric explain(entity)
+
+@defmethod explain(entity::Int) = print("$entity is a Int")
+
+@defmethod explain(entity::Rational) = print("$entity is a Rational")
+
+@defmethod explain(entity::String) = print("$entity is a String")
+
+@defmethod after explain(entity::Int) = print(" (in binary, is $(string(entity, base=2)))")
+
+@defmethod before explain(entity::Real) = print("The number ")
