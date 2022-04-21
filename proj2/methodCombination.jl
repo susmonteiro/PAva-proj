09c258e8fb35bde4d5ca7572b59f0af30544e2e1
susmonteiro <s.moreno.monteiro@tecnico.ulisboa.pt>
2022-04-21 10:57:31 +0100

    save applicable methods in array and start to explore sort

--- proj2/methodCombination.jl
+++ proj2/methodCombination.jl
@@ -15,46 +15,6 @@ end
 
 (f::Generic)(args...) = combineMethods(f, args...)
 
-function no_applicable_method(f::Generic, args...)
-    # todo print types like (x,y) instead of Tuple{x,y}
-    error("No applicable method for arguments $args of types $(typeof(args))")
-end
-
-
-# todo should be done with multiple dispatch instead (standard, tuple, etc should be objects)
-function combineMethods(genericMethod::Generic, arguments...)
-    if genericMethod.qualifier == :standard
-        standardCombination(genericMethod, arguments...)
-    else
-        tupleCombination(genericMethod, arguments...)
-    end
-end
-
-function standardCombination(genericMethod::Generic, arguments...)
-    getApplicableMethods(genericMethod.methods, :before, arguments...)
-    found = getApplicableMethods(genericMethod.methods, :primary, arguments...)
-    if !found
-        no_applicable_method(genericMethod, arguments...)
-    end
-    getApplicableMethods(genericMethod.methods, :after, arguments...)
-end
-
-function tupleCombination(genericMethod::Generic, arguments...)
-    # todo
-end
-
-# todo probably we should have 3 functions, each for the corresponding qualifier and then call them using multiple dispatch
-function getApplicableMethods(methods, qualifier, arguments...)
-    found = false
-    for method in methods
-        if method.qualifier == qualifier && applicable(method.native_function, arguments...)
-            method.native_function(arguments...)
-            found = true
-        end
-    end
-    return found
-end
-
 macro defgeneric(form, qualifier=:standard)
     if qualifier != :standard && qualifier != :tuple
         # ? best exception to throw
@@ -97,6 +57,62 @@ macro defmethod(qualifier, form)
     end
 end
 
+function no_applicable_method(f::Generic, args...)
+    # todo print types like (x,y) instead of Tuple{x,y}
+    error("No applicable method for arguments $args of types $(typeof(args))")
+end
+
+
+# todo should be done with multiple dispatch instead (standard, tuple, etc should be objects)
+function combineMethods(genericMethod::Generic, arguments...)
+    if genericMethod.qualifier == :standard
+        standardCombination(genericMethod, arguments...)
+    else
+        tupleCombination(genericMethod, arguments...)
+    end
+end
+
+function standardCombination(genericMethod::Generic, arguments...)
+    applicable_methods_before = getApplicableMethods(genericMethod.methods, :before, arguments...)
+    callApplicableMethods(applicable_methods_before, arguments...)
+
+    applicable_methods = getApplicableMethods(genericMethod.methods, :primary, arguments...)
+    if isempty(applicable_methods)
+        no_applicable_method(genericMethod, arguments...)
+    else
+        # todo call the most specific instead of the first one
+        first(applicable_methods).native_function(arguments...)
+    end
+
+    applicable_methods_after = getApplicableMethods(genericMethod.methods, :after, arguments...)
+    callApplicableMethods(applicable_methods_after, arguments...)
+end
+
+function tupleCombination(genericMethod::Generic, arguments...)
+    # todo
+end
+
+# todo probably we should have 3 functions, each for the corresponding qualifier and then call them using multiple dispatch
+function getApplicableMethods(methods, qualifier, arguments...)
+    applicable_methods = []
+    for method in methods
+        if method.qualifier == qualifier && applicable(method.native_function, arguments...)
+            push!(applicable_methods, method)
+        end
+    end
+    return applicable_methods
+end
+
+function callApplicableMethods(methods, arguments...)
+    for method in methods
+        method.native_function(arguments...)
+    end
+end
+
+function sortMethods(methods, reverse = false)
+    # ! not working but close (maybe)
+    sort(methods, by = x -> x.parameters, lt = (x,y) -> x <: y, rev = reverse)
+end
 
 
 # todo remove this
@@ -107,6 +123,8 @@ end
 @defmethod explain(entity::Rational) = print("$entity is a Rational")
 
 @defmethod explain(entity::String) = print("$entity is a String")
+
+@defmethod explain(entity::Number) = print(" and a Number")
 
 @defmethod after explain(entity::Int) = print(" (in binary, is $(string(entity, base=2)))")
 
